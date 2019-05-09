8f2c864c1892e0a5ef16aa3bbc7ee5461e33f763
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-05-09 11:43:56 -0300

    Ajustado caminho de arquivos e removendo paginas nao usadas

--- RazorDapper/Pages/Index.cshtml.cs
+++ RazorDapper/Pages/Index.cshtml.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorDapper.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return RedirectToPage("/Produto/Index");
         }
     }
 }
